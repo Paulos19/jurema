aae01b13777550b97fe1f7ba505deacb052417ca
Paulos19 <paulohenrique.012araujo@gmail.com>
2025-07-20 15:57:28 -0300

    jurema

---
+++ app/api/installment/overdue/route.ts
@@ -0,0 +1,55 @@
+// app/api/installments/overdue/route.ts
+
+import { NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
+
+export async function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const uniqueCode = searchParams.get('uniqueCode');
+
+    if (!uniqueCode) {
+      return NextResponse.json({ message: 'O código único do credor é obrigatório' }, { status: 400 });
+    }
+
+    const user = await prisma.user.findUnique({ where: { uniqueCode } });
+    if (!user) {
+      return NextResponse.json({ message: 'Código único inválido' }, { status: 401 });
+    }
+
+    const overdueInstallments = await prisma.installment.findMany({
+      where: {
+        status: 'Atrasado',
+        loan: {
+          userId: user.id,
+        },
+      },
+      include: {
+        loan: {
+          select: {
+            title: true,
+            client: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
+      },
+      orderBy: {
+        dueDate: 'asc',
+      },
+    });
+
+    if (overdueInstallments.length === 0) {
+      return NextResponse.json({ message: 'Nenhuma parcela em atraso encontrada.' }, { status: 404 });
+    }
+
+    return NextResponse.json(overdueInstallments, { status: 200 });
+
+  } catch (error) {
+    console.error('Erro ao buscar parcelas atrasadas:', error);
+    const errorMessage = error instanceof Error ? error.message : 'Erro interno.';
+    return NextResponse.json({ message: 'Erro ao processar a solicitação.', error: errorMessage }, { status: 500 });
+  }
+}
